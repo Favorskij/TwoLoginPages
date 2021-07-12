a1f912a77accfcbf0cdc777b972cbd2f86a934a4
MyTestPerson <romanych_2021@list.ru>
2021-07-12 17:17:22 +1100

    Commit

--- src/main/java/com/twologinpages/controller/UserLoginController.java
+++ src/main/java/com/twologinpages/controller/UserLoginController.java
@@ -1,18 +1,11 @@
 package com.twologinpages.controller;
 
-import com.twologinpages.model.user.User;
-import com.twologinpages.repository.UserJpa;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserLoginController {
-
-    @Autowired
-    UserJpa userJpa;
-
 
     @GetMapping(value = "/user/loginUser")
     public ModelAndView loginGetMethod () {
